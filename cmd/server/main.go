0501f8f71ab261b9f357c094c98cd0e0460cb7ee
Ryohei Kamo <ryohei.kamo0944@gmail.com>
2026-08-26 04:24:36 +0900

    Add Slack URL verification endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type SlackEventRequest struct {
+	Type      string `json:"type"`
+	Challenge string `json:"challenge"`
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "go-slack-bot")
 	})
 
-	log.Println("server started on :8081")
+	http.HandleFunc("/slack/events", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
 
-	if err := http.ListenAndServe(":8081", nil); err != nil {
+		var req SlackEventRequest
+
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+
+		if req.Type == "url_verification" {
+			w.Header().Set("Content-Type", "application/json")
+
+			if err := json.NewEncoder(w).Encode(map[string]string{
+				"challenge": req.Challenge,
+			}); err != nil {
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			}
+
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	log.Println("server started on :8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
